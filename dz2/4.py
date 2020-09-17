e81f93008da71d495002f30e6164b0130aaed5b8
romanzanin <55848223+romanzanin@users.noreply.github.com>
2020-09-17 14:59:28 +0300

    Create 4.py

---
+++ dz2/4.py
@@ -0,0 +1,56 @@
+def bracket(f):
+
+    open_b = 0
+    
+    end_b = 0
+    
+    open_b = f.count("(")
+    
+    end_b = f.count(")")
+    
+    k = str(end_b - open_b)
+    
+    m = str(open_b - end_b)
+    
+    if end_b > open_b:
+    
+        if k == 1:
+        
+            return ("You miss the opening 1 bracket")
+            
+        else:
+        
+            return ( "You miss the opening" + " " + k + " " + "brackets")
+            
+    elif end_b < open_b:
+    
+        if m == 1:
+        
+            return ("You miss the ending 1 bracket")
+            
+        else:
+        
+            return ("You miss the ending" + " " + m + " " + "brackets")
+            
+    elif open_b == open_b == 0:
+    
+        return("You forgot to write something (((")
+        
+    else: 
+            return ("Well done!")
+            
+a = input()
+
+lst = []
+
+while a: 
+
+    lst.append(a)
+    
+    a = input()
+    
+m = str ()
+
+m = bracket(lst)
+
+print(m)
