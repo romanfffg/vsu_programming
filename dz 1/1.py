7323006e781391d2192e5d2ecc0e30c32e1080b5
romanzanin <55848223+romanzanin@users.noreply.github.com>
2020-09-17 14:40:15 +0300

    Update and rename ht1 to 1.py

---
+++ dz 1/1.py
@@ -0,0 +1,14 @@
+ a = float(input())
+
+ b = float(input())
+
+ if a > b:
+     print('first number more than second')
+    
+ elif a < b:
+
+     print('second number more than first')
+    
+ else:
+
+    print('It is equality')
