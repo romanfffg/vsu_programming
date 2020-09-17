95121fb3f45ca4a9a20c2fc645bcc07143235e75
romanzanin <55848223+romanzanin@users.noreply.github.com>
2020-09-17 14:50:23 +0300

    Create 6.py

---
+++ dz 1/6.py
@@ -0,0 +1,23 @@
+a = float(input())
+
+b = float(input())
+
+if a > 0 and b > 0:
+
+    print('1/4 on 1')
+    
+elif a < 0 and b > 0:
+
+    print('1/4 on 2')
+    
+elif a < 0 and b < 0 :
+
+    print('1/4 on 3')
+    
+elif a > 0 and b < 0:
+
+    print('1/4 on 4')
+    
+else:
+
+    print('Point on axis')
