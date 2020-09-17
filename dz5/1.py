009e4a5379ccec18cd6bbb7e521388ca7cb81898
romanzanin <55848223+romanzanin@users.noreply.github.com>
2020-09-17 16:04:56 +0300

    Create 1.py

---
+++ dz5/1.py
@@ -0,0 +1,36 @@
+from collections import deque
+
+name = {
+
+    "Roman": ["Lily", "Sophy"],
+
+    "Lily": ["Amy", "Bob"],
+
+    "Bob": ["Darel", "Dima", "Sasha"]
+
+}
+def doter(x):
+
+    return not len(x) % 2 and x[0] == "D"
+
+a = deque(name["Pavel"])
+
+b = []
+
+while name:
+
+    person = a.popleft()
+
+    if person not in b:
+
+        if doter(person):
+
+            print(person)
+
+            break
+
+        else:
+
+            a += name.get(person, [])
+
+        b.append(person)
